a626ec23a4ce9d397a0f5c76c53c357ac047d32d
LivinkumarS <001liv100@ggmail.com>
2025-07-22 12:45:05 +0530

    Day 3

---
+++ Day3.sql
@@ -0,0 +1,23 @@
+
+USE company;
+UPDATE student_list SET age=30 WHERE studentname='Sindhu' OR studentname='Dhivya';
+
+SELECT * FROM student_list LIMIT 10 OFFSET 2;
+
+USE company;
+SELECT * FROM student_list where age>40 ORDER BY studentname desc LIMIT 2;
+
+
+USE company;
+CREATE TABLE actor_list(
+id INT PRIMARY KEY auto_increment,
+name VARCHAR(50) NOT NULL,
+email VARCHAR(100) NOT NULL UNIQUE,
+age INT not null,
+isMarried BOOLEAN );
+
+
+INSERT INTO actor_list (name, email, age, isMarried) VALUES ('Vijay Sethupathy','vjs@gamil.com',55,True),('Harish Kalyan','hk@gamil.com',2,True),('Trisha','Tri@gamil.com',42,False),('Arjun Das','ad@gamil.com',32,False);
+
+SELECT id,name,email,age,isMarried FROM actor_list;
+UPDATE actor_list SET age=32;
